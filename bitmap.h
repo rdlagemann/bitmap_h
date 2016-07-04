574acfe08b88677a6e7fa9785f90471bf7833921
Rafael Lagemann <rdlagemann@gmail.com>
2016-07-04 15:40:33 -0300

    First commit

---
+++ bitmap.h
@@ -0,0 +1,53 @@
+/** 
+ * Author: Rafael Lagemann
+ * E-mail: rdlagemann@gmail.com
+ * Computer Science
+ * Federal University of Pelotas - Brazil
+ * 06-22-2016 
+ * 
+ * */
+#include <stdio.h>
+#include <math.h> //in some OS's, pay attention to -lm flag to build it right
+
+#ifndef BITMAP_H	
+#define BITMAP_H
+
+typedef unsigned char bitmap_t;
+
+/**Description		:Change of set to unset and vice versa
+ * 
+ * @param bitmap	:pointer to a bitmap
+ * @param index		:position of the bit in the bitmap (left to right, 0 to 7)
+ * @return 			:1 if succeeds, 0 if don't
+ *  
+ */
+ int change_bitmap(bitmap_t *bitmap, int index)
+{
+	if((index < 0) || (index > 7)) return 0; //fail - index out of range
+	*bitmap = *bitmap ^ (1 << (index & 7));
+	return 1;
+}
+
+/**Description	 :Get bit value from given bitmap
+ * 
+ * @param bitmap :pointer to a bitmap
+ * @param index  :position of the bit in the bitmap (left to right, 0 to 7)
+ * @return the bit 
+ * 
+ */
+int get_bitmap(bitmap_t *bitmap, int index)
+{
+	
+	if((index < 0) || (index > 7))
+		return -1; //fail - index out of range
+		 
+	int comparator;
+	comparator = *bitmap & (1 << (index & 7));
+		
+	if((int)pow(2,index) == comparator)
+		return 1;
+	else
+		return 0;		
+}
+
+#endif
---
+++ bitmapsample.c
@@ -0,0 +1,96 @@
+/** 
+ * Author: Rafael Lagemann
+ * E-mail: rdlagemann@gmail.com
+ * Computer Science
+ * Federal University of Pelotas - Brazil
+ * 06-22-2016 
+ * 
+ * */
+#include <stdio.h>
+#include "bitmap.h"
+
+/**
+ * printBits() writen by Stack Overflow user "user295190" 
+ * here: stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
+ * 
+ * We'll use it for bitmap better visualization
+ * 
+ * */
+void printBits(size_t const size, void const * const ptr); 
+
+int main (void)
+{
+	bitmap_t myBitmap = {0}; //bitmap all setted in 0
+	int bitmap_bit;
+	
+	puts("Initial Bitmap:"); 
+	printBits(sizeof(myBitmap), &myBitmap);
+	
+	change_bitmap(&myBitmap, 0); //set index 0
+	puts("First Bitmap config:");
+	printBits(sizeof(myBitmap), &myBitmap);
+	
+	change_bitmap(&myBitmap, 7); //set index 7
+	puts("Second Bitmap config:");
+	printBits(sizeof(myBitmap), &myBitmap);
+	
+	change_bitmap(&myBitmap, 7); //unset index 7
+	puts("Third Bitmap config:");
+	printBits(sizeof(myBitmap), &myBitmap);
+	
+	/**
+	 * Remember change_bitmap(...) will return 0(FAIL) if a index is out of range
+	 * So, you can go like this:
+	 * */
+	
+	if(!change_bitmap(&myBitmap, 4)) //success
+	{
+		puts("Index out of range");
+		return 1;
+	}
+	puts("Fourth Bitmap config:");
+	printBits(sizeof(myBitmap), &myBitmap);
+	
+	if(!change_bitmap(&myBitmap, 23))//fail
+		puts("Bit 23 = Index out of range");
+	
+	bitmap_bit = get_bitmap(&myBitmap, 0);
+	printf("Bit 0 = %i\n", bitmap_bit);
+	
+	bitmap_bit = get_bitmap(&myBitmap, 1);
+	printf("Bit 1 = %i\n", bitmap_bit);
+	
+	bitmap_bit = get_bitmap(&myBitmap, 4);
+	printf("Bit 4 = %i\n", bitmap_bit);
+	
+	bitmap_bit = get_bitmap(&myBitmap, 7);
+	printf("Bit 7 = %i\n", bitmap_bit);
+	
+	//now for a invalid index
+	bitmap_bit = get_bitmap(&myBitmap, 23);
+	printf("Bit 23 = %i //so you can verify if bitmap_bit > 0\n", bitmap_bit);
+	
+	//correctly
+	if((bitmap_bit = get_bitmap(&myBitmap, 23)) < 0) 
+		puts("Bit 23 = Index out of range"); //this is kinda ugly
+		
+	
+	return 0;
+}
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
